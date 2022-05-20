b1f4565aa9be4d6cca192d23c0ba5ec0e7b0fe01
SIVA AN <66514135+siva-hut@users.noreply.github.com>
2022-05-20 14:29:23 +0530

    Adding comments

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 
 // Main function
 func main() {
-
+	//Create database connection
 	con1()
+	//populate the database with data from like and users JSON
 	populate()
+	//Start server and route the API's
 	route()
 }
