62b07210ab1cf5f2fc1f79e26b583f5674846f7e
SIVA AN <66514135+siva-hut@users.noreply.github.com>
2022-05-20 14:30:16 +0530

    Adding comments

--- outmodels.go
+++ outmodels.go
@@ -1,11 +1,11 @@
 package main
-
+// Return Type for Matches API
 type match struct {
 	Status  string `json:"status"`
 	Data    []like `json:"data"`
 	Message string `json:"message"`
 }
-
+// Return Type for Getusers and find API
 type finduser struct {
 	Status  string `json:"status"`
 	Data    []User `json:"data"`
