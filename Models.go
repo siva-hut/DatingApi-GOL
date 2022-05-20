e0cbea7352e229485ea00ce8256677922bf6e86b
SIVA AN <66514135+siva-hut@users.noreply.github.com>
2022-05-20 14:26:20 +0530

    adding comments

--- Models.go
+++ Models.go
@@ -1,11 +1,11 @@
 package main
-
+// Like table: Unique ID, contains who likes who
 type like struct {
 	Id           uint `json:"id" gorm:"primaryKey"`
 	Who_likes    int  `json:"who_likes"`
 	Who_is_liked int  `json:"who_is_liked"`
 }
-
+// User entity
 type User struct {
 	Id       uint    `json:"id" gorm:"primaryKey"`
 	Name     string  `json:"name"`
@@ -13,11 +13,12 @@ type User struct {
 	Gender   string  `json:"gender"`
 	Email    string  `json:"email"`
 }
-
+// Get POST Json for API type getsuers
 type getuser struct {
 	Id       uint    `json:"id"`
 	Distance float32 `json:"distance"`
 }
+// Helper struct to find matches
 type helper struct {
 	Id1 uint
 	Id2 uint
